6a5e83509bfcfb4fac445890176ee5ef154330f5
Shreyas Shinde <130788791+shreyasShinde1@users.noreply.github.com>
2023-07-31 22:54:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,10 +8,13 @@ const person = {
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 let team=players;
 let team1=[];
-team.forEach(function (team2){
+team.forEach(function (player){
 	team1.push(player);
 })
 let cap1=Object.assign({}, person);
+
+alert(cap1,team1);
+
 window.players = players;
 window.person = person;
 window.team = team;
